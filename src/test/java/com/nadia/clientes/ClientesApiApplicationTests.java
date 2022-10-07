28ac08dc9b31e7e270cff741b3a70dff19e4bd87
Nadia <nadiatandrade@gmail.com>
2022-10-07 08:55:16 -0300

    Project's setup

---
+++ src/test/java/com/nadia/clientes/ClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadia.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
